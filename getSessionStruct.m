881bc568a36bfd66ed4340a4aeafcfe89fc3e654
Eduardo Rojas Hortelano <edrojas3@gmail.com>
2018-01-31 10:48:15 -0600

    Sample data
    Data from neurons with preference of rotation direction

--- getSessionStruct.m
+++ getSessionStruct.m
@@ -1,36 +1,37 @@
 function e = getSessionStruct(id)
-% USO: e = getSessionStruct(id)
-% id = el identificador del archivo a cargar ej. d1605131058
-% e = estructura con la información de la sesión
+% USAGE: e = getSessionStruct(id)
+% id = file identifier ej. d1605131058
+% e = struct with session info separated by trial
 
-%%
- nevData = ['C:\Users\eduardo\Google Drive\Exp mono\',id,'.nev'];
- ns1File = ['C:\Users\eduardo\Google Drive\Exp mono\', id, '.ns1'];
- 
-e = blackRock2event(nevData,ns1File);
-if isstruct(e)
-    e = addRobMarkers(e);
+% File path
+nevFile = ['datafiles\',id,'.nev'];
+ns1File = ['datafiles\', id, '.ns1'];
+ns2File = (['datafiles\', id,'.ns2']);
 
-
-    nevdir = 'C:\Users\eduardo\Google Drive\Exp mono';
-    ns2File = ([nevdir, '\', id,'.ns2']);
-    ns2 = openNSx(ns2File);
-    lfp = ns2.Data;
-
-    % Crear eje de tiempo para cada muestra del LFP
-    fs = ns2.MetaTags.SamplingFreq;
-    timeSecs = (1:size(lfp,2))/fs;
-
-    % Cortar la señal del LFP usando el inicio y el final de cada ensayo
-    for n = 1:length(e.trial);
-        trialStart = e.trial(n).waitCueIni - 1;
-        trialEnd = e.trial(n).targOff + 1;
-        timeIndex = timeSecs >= trialStart & timeSecs <= trialEnd;
-        lfpSection = lfp(:,timeIndex == 1);
-        timeSection = timeSecs(timeIndex == 1);
-        e.trial(n).lfp = lfpSection;
-        e.trial(n).lfpTime = timeSection;
+% Session structure
+if exist(nevFile,'file') && exist(ns1File,'file');
+    e = blackRock2event(nevFile,ns1File); % event time-stamps and spikes
+    if isstruct(e)
+        e = addRobMarkers(e); % Add markers of the stimulus movement
+        % Add LFP signal to the structure
+        if exist(ns2File,'file')
+            ns2 = openNSx(ns2File);
+            lfp = ns2.Data;
+            fs = ns2.MetaTags.SamplingFreq;
+            timeSecs = (1:size(lfp,2))/fs; 
+            for n = 1:length(e.trial);
+                trialStart = e.trial(n).waitCueIni - 1;
+                trialEnd = e.trial(n).targOff + 1;
+                timeIndex = timeSecs >= trialStart & timeSecs <= trialEnd;
+                lfpSection = lfp(:,timeIndex == 1);
+                timeSection = timeSecs(timeIndex == 1);
+                e.trial(n).lfp = lfpSection;
+                e.trial(n).lfpTime = timeSection;
+            end
+        else
+            warning('No ns2 file found. LFP will not be added to session structure');
+        end
     end
-
-
+else
+    error('No nev or ns1 file found.')
 end
